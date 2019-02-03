15bbe5ca987a24cdc1919af2e56b96c68fbb8e78
Martin Steen Nielsen <martin.s.nielsen@simcorp.com>
2019-02-03 12:00:53 +0100

    Basic constants and CRT modelling

--- Program.cs
+++ Program.cs
@@ -1,46 +1,67 @@
-﻿using OpenTK;
+﻿using System.Collections.Generic;
+using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Input;
 
-namespace CompositeVideoMonitor
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (Game game = new Game(800, 600, "LearnOpenGL"))
-            {
-                //To create a new window, create a class that extends GameWindow, then call Run() on it.
-                //Run takes a double, which is how many frames per second it should strive to reach.
-                //You can leave that out and it'll just update as fast as the hardware will allow it.
-                game.Run(60.0);
-            }
+namespace CompositeVideoMonitor {
 
-            //And that's it! That's all it takes to create a window with OpenTK.
+    class Program {
+        static void Main(string[] args) {
+            using (VideoMonitor monitor = new PalMonitor(800, 600, "PAL")) {
+                monitor.Run(60.0);
+            }
         }
-
-    }
-
-    //This is where all OpenGL code will be written.
-    //OpenTK allows for several functions to be overriden to extend functionality; this is how we'll be writing code.
-    class Game : GameWindow
-    {
-        //A simple constructor to let us set the width/height/title of the window.
-        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
-
-        //This function runs on every update frame.
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-            //Get the current state of the keyboard on this frame.
-            KeyboardState input = Keyboard.GetState();
-
-            //Check if the Escape button is currently being pressed.
-            if (input.IsKeyDown(Key.Escape))
-            {
-                //If it is, exit the window.
-                Exit();
+        class PalMonitor : VideoMonitor {
+            public PalMonitor(int width, int height, string title) : base(width, height, title, hSyncFreq: 15625, vSyncFreq: 25, bandwidthFreq: 5e6) {
             }
-            base.OnUpdateFrame(e);
+        }
+        
+        class VideoMonitor : GameWindow {
+            CRT Screen;
+            double VSyncFreq;
+            double HSyncFreq;
+            double NoOfLines;
+            double SampleTime;
+
+            public VideoMonitor(int width, int height, string title, double hSyncFreq, double vSyncFreq,double bandwidthFreq) : base(width, height, GraphicsMode.Default, title) {
+                Screen = new CRT(height:0.03, width:0.04, deflectionVoltage: 400);
+                VSyncFreq = vSyncFreq;
+                HSyncFreq = hSyncFreq;
+                NoOfLines = hSyncFreq/vSyncFreq;
+                SampleTime = 1 / bandwidthFreq;
+            }
+
+            protected override void OnUpdateFrame(FrameEventArgs e) {
+                KeyboardState input = Keyboard.GetState();
+                if (input.IsKeyDown(Key.Escape)) {
+                    Exit();
+                }
+                base.OnUpdateFrame(e);
+            }
+        }
+        class CRT {
+            readonly double Width;
+            readonly double Height;
+            readonly double HDeflection;
+            readonly double VDeflection;
+            readonly double PhosphorPersistenseTime = 0.1;
+            readonly double PhosphorBleedDistance = 0.001;
+            readonly HashSet<PhosphorExcitation> Excitations;
+            double HPos(double hVoltage) => hVoltage*HDeflection;
+            double VPos(double vVoltage) => vVoltage*VDeflection;
+
+            public CRT(double height, double width, double deflectionVoltage) {
+                VDeflection = height/deflectionVoltage;
+                HDeflection = width/deflectionVoltage;
+                Excitations = new HashSet<PhosphorExcitation>();
+            }
+
+            class PhosphorExcitation {
+                double VSyncVoltage;
+                double HSyncVoltage;
+                double StartTime;
+                double StartLevel;
+            }
         }
     }
 }
