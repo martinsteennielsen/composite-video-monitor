2df034ebb4348c83d55c7711db02e201c5ea7f07
msi <martin.s.nielsen@simcorp.com>
2019-02-09 00:39:53 +0100

    Focus control key D,F

--- Renderer.cs
+++ Renderer.cs
@@ -11,7 +11,7 @@ namespace CompositeVideoMonitor {
         readonly VideoMonitor CRT;
         readonly Logger Logger;
         readonly double ScaleX, ScaleY;
-        readonly double Focus = 1;
+        double Focus = 1;
         readonly double Slope, DotWidth, DotHeight;
 
         public Renderer(VideoMonitor monitor, Timing timing, Logger logger, int width, int height, string title) : base(width, height, GraphicsMode.Default, title) {
@@ -19,10 +19,16 @@ namespace CompositeVideoMonitor {
             Timing = timing;
             Logger = logger;
             ScaleX = 2.0 / CRT.TubeWidth;
-            ScaleY = -2.0 / CRT.TubeHeight;
+            ScaleY = 2.0 / CRT.TubeHeight;
             KeyDown += (_, e) => {
                 if (e.Key == Key.Escape) {
                     Exit();
+                }
+                if (e.Key == Key.F) {
+                    Focus *= 1.05;
+                }
+                if (e.Key == Key.D) {
+                    Focus /= 1.05;
                 }
             };
             GL.Enable(EnableCap.Blend);
@@ -33,7 +39,7 @@ namespace CompositeVideoMonitor {
             double dx = CRT.HPos(hOsc.Get(Timing.DotTime)) - CRT.HPos(hOsc.Get(0));
             double dy = CRT.VPos(vOsc.Get(Timing.LineTime)) - CRT.VPos(vOsc.Get(0));
             DotWidth = ScaleX * dx;
-            DotHeight= -ScaleY * dy;
+            DotHeight= ScaleY * dy;
             Slope = Math.Atan2( CRT.VPos(vOsc.Get(0)) - CRT.VPos(vOsc.Get(Timing.DotTime)), dx );
         }
 
@@ -48,13 +54,13 @@ namespace CompositeVideoMonitor {
             double dx = 0.5 * Focus * DotWidth;
             double dy = 0.5 * Focus * DotHeight;
             foreach (var dot in dots.SelectMany(x => x.Dots)) {
-                var dotLifeTime = 1 - (simulationTime - dot.Time) / CRT.PhosphorGlowTime;
-                if (dotLifeTime < 0) continue;
-                if (dotLifeTime > 1) continue;
-                double brightness = dot.Brightness * dotLifeTime;
+                //var dotLifeTime = 1 - (simulationTime - dot.Time) / CRT.PhosphorGlowTime;
+                //if (dotLifeTime < 0) continue;
+                //if (dotLifeTime > 1) continue;
+                double brightness = dot.Brightness / (Focus*Focus);
                 GL.Color4(brightness, brightness, brightness, 0.3);
                 double xPos = ScaleX*CRT.HPos(dot.HVolt);
-                double yPos = ScaleY*CRT.VPos(dot.VVolt);
+                double yPos = -ScaleY*CRT.VPos(dot.VVolt);
                 GL.Vertex2( xPos - dx, yPos + dy);
                 GL.Vertex2( xPos + dx, yPos + dy);
                 GL.Vertex2( xPos + dx, yPos - dy);
