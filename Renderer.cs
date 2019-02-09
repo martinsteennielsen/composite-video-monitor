6b03b4b2131c87e6f11b62cf322b7846aff11a4a
Martin Steen Nielsen <martin.s.nielsen@simcorp.com>
2019-02-09 17:58:39 +0100

    Cleanup

--- Renderer.cs
+++ Renderer.cs
@@ -12,7 +12,7 @@ namespace CompositeVideoMonitor {
         readonly Logger Logger;
         readonly double ScaleX, ScaleY;
         readonly double Focus = 1;
-        readonly double Slope, DotWidth, DotHeight;
+        readonly double DotWidth, DotHeight;
 
         public Renderer(VideoMonitor monitor, Timing timing, Logger logger, int width, int height, string title) : base(width, height, GraphicsMode.Default, title) {
             CRT = monitor;
@@ -20,21 +20,20 @@ namespace CompositeVideoMonitor {
             Logger = logger;
             ScaleX = 2.0 / CRT.TubeWidth;
             ScaleY = -2.0 / CRT.TubeHeight;
+
             KeyDown += (_, e) => {
                 if (e.Key == Key.Escape) {
                     Exit();
                 }
             };
+
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactorSrc.SrcColor, BlendingFactorDest.DstColor);
+
             var hOsc = new SawtoothSignal(frequency: timing.HFreq, phase: 0);
             var vOsc = new SawtoothSignal(frequency: timing.VFreq, phase: 0);
-
-            double dx = CRT.HPos(hOsc.Get(Timing.DotTime)) - CRT.HPos(hOsc.Get(0));
-            double dy = CRT.VPos(vOsc.Get(Timing.LineTime)) - CRT.VPos(vOsc.Get(0));
-            DotWidth = ScaleX * dx;
-            DotHeight= -ScaleY * dy;
-            Slope = Math.Atan2( CRT.VPos(vOsc.Get(0)) - CRT.VPos(vOsc.Get(Timing.DotTime)), dx );
+            DotWidth = ScaleX * CRT.HPos(hOsc.Get(Timing.DotTime)) - CRT.HPos(hOsc.Get(0));
+            DotHeight= -ScaleY * CRT.VPos(vOsc.Get(Timing.LineTime)) - CRT.VPos(vOsc.Get(0));
         }
 
         protected override void OnRenderFrame(FrameEventArgs e) {
@@ -42,16 +41,14 @@ namespace CompositeVideoMonitor {
             GL.ClearColor(Color.Black);
             GL.Clear(ClearBufferMask.ColorBufferBit);
             GL.Flush();
+
             GL.Begin(PrimitiveType.Quads);
             var dots = CRT.GetDots();
             double simulationTime = CRT.SimulatedTime;
             double dx = 0.5 * Focus * DotWidth;
             double dy = 0.5 * Focus * DotHeight;
             foreach (var dot in dots.SelectMany(x => x.Dots)) {
-                var dotLifeTime = 1 - (simulationTime - dot.Time) / CRT.PhosphorGlowTime;
-                if (dotLifeTime < 0) continue;
-                if (dotLifeTime > 1) continue;
-                double brightness = dot.Brightness * dotLifeTime;
+                double brightness = dot.Brightness;
                 GL.Color4(brightness, brightness, brightness, 0.3);
                 double xPos = ScaleX*CRT.HPos(dot.HVolt);
                 double yPos = ScaleY*CRT.VPos(dot.VVolt);
@@ -61,6 +58,7 @@ namespace CompositeVideoMonitor {
                 GL.Vertex2( xPos - dx, yPos - dy);
             }
             GL.End();
+
             SwapBuffers();
 
             Logger.DotCount = dots.Sum(x => x.Dots.Count);
