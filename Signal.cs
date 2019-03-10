44f480a05b800e4ec34633cc822f52ef25f778ca
martinsteennielsen <martin.steen.nielsen@gmail.com>
2019-03-10 17:57:47 +0100

    Make signal function faster to calculate

--- Signal.cs
+++ Signal.cs
@@ -6,20 +6,29 @@ namespace CompositeVideoMonitor {
         double Get(double time);
     }
 
-    public interface IPeriodic : ISignal {
-        double Frequency { get; set; }
-        double Phase { get; set; }
-    }
-
     public class NoiseSignal : ISignal {
         Random Randomizer = new Random();
         public double Get(double time) => Randomizer.NextDouble();
     }
 
+    public interface IPeriodic : ISignal {
+        double Frequency { get; set; }
+        double Phase { get; set; }
+    }
+
     public class SawtoothSignal : IPeriodic {
-        readonly double Pi = Math.PI;
-        public double Frequency { get; set; }
-        public double Phase { get; set; }
-        public double Get(double time) => 2.0 / Pi * (Frequency * Pi * ((time + Phase/Frequency/Math.PI) % (1.0 / Frequency)) - (Pi / 2.0));
+        double period, frequency, angularShift, phase;
+
+        public double Frequency {
+            get => frequency;
+            set { frequency = value; period = 1d / value; }
+        }
+
+        public double Phase {
+            get => phase;
+            set { phase = value; angularShift = value / frequency / Math.PI; }
+        }
+
+        public double Get(double time) => -1 + 2 * frequency * ((time + angularShift) % period);
     }
 }
